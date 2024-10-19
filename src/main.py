e067bc1c1495960af529d97c4b6d0f8af1c028e4
Sree Duggirala <sreevanthduggirala@gmail.com>
2024-10-19 14:42:02 -0700

    comment out hf code

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+from openai import OpenAI
 import os
-from transformers import pipeline
 
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline(
-    "text-generation",
-    model="meta-llama/Llama-3.1-8B-Instruct",
-    token=os.environ["HF_PAT"],
-)
-pipe(messages)
+# from transformers import pipeline
+
+# messages = [
+#     {"role": "user", "content": "Who are you?"},
+# ]
+# pipe = pipeline(
+#     "text-generation",
+#     model="meta-llama/Llama-3.1-8B-Instruct",
+#     token=os.environ["HF_PAT"],
+# )
+# pipe(messages)

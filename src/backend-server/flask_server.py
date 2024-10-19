d4e86969443a2212a09c8ebee98fbe17f357c9de
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-10-19 13:32:59 -0500

    added dummy functions to the flask web server.. this will be until sree adds in actual code for RLHF

--- src/backend-server/flask_server.py
+++ src/backend-server/flask_server.py
@@ -14,8 +14,23 @@ def hello_api():
     return {'hello': 'world'}
 
 
+# 1) quereying these models and getting the answers frm the model
+# input: model name
+# input: prompt
+# output: model output
+@app.route('/api/inference/<model>/<prompt>')
+def inference(model, prompt):
+    # query the model and get the output
+    return {'model': model, 'prompt': prompt, 'output': f('OUTPUT OUTPOUT OUTPUT HEHE +'+model+"SAD"+prompt)}
 
 
+# 2) updating RLHF given human feedback
+# input: human feedback (Usefulness, Clarity Relevance) all frm - (0->10)
+# output: new model weights
+@app.route('/api/feedback/<usefulness>/<clarity>/<relevance>')
+def feedback(usefulness, clarity, relevance):
+    # update the model weights
+    return {'usefulness': usefulness, 'clarity': clarity, 'relevance': relevance, 'output': 'new model weights'}
 
 
 

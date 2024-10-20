dcf3f8afe0aa54fe06562f5df4eea8285102fc89
Sree Duggirala <sreevanthduggirala@gmail.com>
2024-10-19 22:39:18 -0700

    update mixtral output

--- main.py
+++ main.py
@@ -4,29 +4,15 @@ import os
 
 client = InferenceClient(api_key=os.environ["HF_PAT"])
 
-# for message in client.chat_completion(
-#     model="mistralai/Mixtral-8x7B-Instruct-v0.1",
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "You are a professional standup comedian who makes jokes about crypto and blockchain.",
-#         }
-#     ],
-#     max_tokens=500,
-#     stream=True,
-# ):
-#     print(message.choices[0].delta.content, end="")
-
-print(
-    client.chat_completion(
-        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        messages=[
-            {
-                "role": "user",
-                "content": "You are a professional standup comedian who makes jokes about crypto and blockchain.",
-            }
-        ],
-        max_tokens=500,
-        stream=True,
-    )
-)
+for message in client.chat_completion(
+    model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+    messages=[
+        {
+            "role": "user",
+            "content": "You are a professional standup comedian who makes jokes about crypto and blockchain.",
+        }
+    ],
+    max_tokens=500,
+    stream=True,
+):
+    print(message.choices[0].delta.content, end="")
